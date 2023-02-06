e393e4eb90118dff6d91ca4f9b6d2988f6ff4d41
Priyanshu Jha <95862487+AstroDeveloper1010@users.noreply.github.com>
2023-02-07 02:01:53 +0530

    Add files via upload

---
+++ javacscript/first-section.js
@@ -0,0 +1,19 @@
+const circuit_simulator_fs = document.querySelector(
+  "#electric-circuit-simulator-first-section"
+);
+const ohms_law_calculator_fs = document.querySelector(
+  "#ohms-law-calculator-first-section"
+);
+const documentation_fs = document.querySelector("#documentation-first-section");
+
+circuit_simulator_fs.addEventListener("click", () => {
+  location.href = "circuit/circuit-nav.html";
+});
+
+ohms_law_calculator_fs.addEventListener("click", () => {
+  location.href = "";
+});
+
+documentation_fs.addEventListener("click", () => {
+  location.href = "";
+});
