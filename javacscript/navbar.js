3a9aa924eb2bb025f985e08d670a6ef68ae000fb
Priyanshu Jha <95862487+AstroDeveloper1010@users.noreply.github.com>
2023-02-06 19:18:26 +0530

    Add files via upload

---
+++ javacscript/navbar.js
@@ -0,0 +1,27 @@
+const home_nav = document.querySelector(".home-nav");
+const circuit_nav = document.querySelector(".circuit-simulation-nav");
+const resourses_nav = document.querySelector(".resources-nav");
+const about_nav = document.querySelector(".about-nav");
+
+home_nav.addEventListener("click", () => {
+  location.href = "src/home-nav.html";
+});
+
+circuit_nav.addEventListener("click", () => {
+  location.href = "src/circuit-nav.html";
+});
+
+about_nav.addEventListener("click", () => {
+  location.href = "src/about-nav.html";
+});
+
+resourses_nav.addEventListener("click", () => {
+  location.href = "src/reviews.html";
+});
+
+
+about_nav = document.addEventListener("click", () => {
+    about_nav.addEventListener("hover", () => {
+        location.reload();
+    })
+})
