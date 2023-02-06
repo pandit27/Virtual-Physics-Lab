116f3c68ee107e6f2aced241b31b588339fbbbed
Priyanshu <priyanshujha131@gmail.com>
2023-02-06 19:19:11 +0530

    new commit

---
+++ javacscript/login-button.js
@@ -0,0 +1,7 @@
+document.querySelector(".my-login-button").addEventListener("click", () => {
+       location.href = 'src/login-signup-page.html';
+    });
+
+document.querySelector(".my-signup-button").addEventListener("click", () => {
+       location.href = 'src/login-signup-page.html';
+    });
