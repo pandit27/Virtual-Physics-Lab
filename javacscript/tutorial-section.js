cd0e0260cc0e5fd21b302dd81fb6516092085d31
Priyanshu Jha <95862487+AstroDeveloper1010@users.noreply.github.com>
2023-02-08 00:53:00 +0530

    Update tutorial-section.js

--- javacscript/tutorial-section.js
+++ javacscript/tutorial-section.js
@@ -1,5 +1,6 @@
 const t1 = document.querySelector(".t-1");
 
 t1.addEventListener("click", () => {
-    location.href = 'https://www.youtube.com/watch?v=AOADrgWdzrU&ab_channel=Scholarswing';
-})
+  location.href =
+    "https://www.youtube.com/watch?v=AOADrgWdzrU&ab_channel=Scholarswing";
+});
