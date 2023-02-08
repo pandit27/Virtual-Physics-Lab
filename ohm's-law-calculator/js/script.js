9aff301e51464847725068b3e2716763a493ad9a
Priyanshu Jha <95862487+AstroDeveloper1010@users.noreply.github.com>
2023-02-08 12:58:38 +0530

    Update script.js

--- ohm's-law-calculator/js/script.js
+++ ohm's-law-calculator/js/script.js
@@ -1,5 +1,5 @@
 const home_btn = document.querySelector(".home-btn-ohms-law");
 
 home_btn.addEventListener("click", () => {
-    location.href = '/index.html';
-})
+  location.href = "../index.html";
+});
