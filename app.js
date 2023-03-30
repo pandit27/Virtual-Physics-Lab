330bd1208004a6832b48bc2f7d72dec7f05f9d83
Priyanshu Jha <priyanshujha131@gmail.com>
2023-03-30 21:12:35 +0530

    updated

--- app.js
+++ app.js
@@ -5,32 +5,6 @@ const { requireAuth } = require('./middlewares/auth');
 
 const app = express();
 
-// Connect to MongoDB
-connectDB();
-
-// Parse incoming requests as JSON
-app.use(express.json());
-
-// Handle user authentication routes
-app.use('/api/auth', authRoutes);
-
-// Example protected route
-app.get('/api/protected', requireAuth, (req, res) => {
-    res.send(`Welcome, ${req.user.username}!`);
-});
-
-// Handle 404 errors
-app.use((req, res) => {
-    res.status(404).json({ message: 'Not found' });
-});
-
-// Handle errors
-app.use((err, req, res, next) => {
-    console.error(err);
-    res.status(500).json({ message: 'Server error' });
-});
-
-// Start the server
 const port = 4000;
 app.listen(port, () => {
     console.log('Server started on port ' + port);
